fb7c5168efdd84ab3efad28b21575e07ddea1c7d
Achuth07 <achuthchandra@gmail.com>
2025-09-22 14:56:15 -0400

    Update login pop-up

--- client/src/components/login-popup.tsx
+++ client/src/components/login-popup.tsx
@@ -51,7 +51,7 @@ export function LoginPopup({ onLogin, onContinueAsGuest }: LoginPopupProps) {
           </p>
           <div className="flex justify-center">
             <img 
-              src="/favicon/android-chrome-512x512.png" 
+              src="/android-chrome-512x512.png" 
               alt="WhatCyber Logo" 
               className="w-16 h-16 rounded-lg"
             />
