f5f934761088c5e192058842ad9fd8e532c2e796
Achuth07 <achuthchandra@gmail.com>
2025-11-20 16:43:04 -0500

    Add Vercel Speed Insights to the application

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")!).render(
@@ -9,6 +10,7 @@ createRoot(document.getElementById("root")!).render(
     <HelmetProvider>
       <App />
       <Analytics />
+      <SpeedInsights />
     </HelmetProvider>
   </>
 );
