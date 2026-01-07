1bc2870c166a294aba35f8021d7da4b2080d74a2
Achuth07 <achuthchandra@gmail.com>
2026-01-07 01:09:32 -0500

    chore: add Sanity `appId` to CLI configuration

--- studio-whatcyber/sanity.cli.ts
+++ studio-whatcyber/sanity.cli.ts
@@ -1,4 +1,4 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
@@ -11,5 +11,6 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/cli#auto-updates
      */
     autoUpdates: true,
+    appId: 'gdqccmm1k5a3q3cga9b6q6kq',
   }
 })
